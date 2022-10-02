b14d5c09feb8b6bfde9b8955251baa1e14a68d73
Shozab Anwar Siddique <shozab.anwar_siddique@kcl.ac.uk>
2022-10-02 18:23:21 +0100

    Solution to median.py

--- median.py
+++ median.py
@@ -9,4 +9,17 @@ while True:
         print("Some input could not be converted to a number!")
     else:
         break
+        
+int first_num = numbers[0]
+int last_num  = numbers[len(numbers)-1]
+
+while first_num < last_num:
+    first_num++
+    last_num--
+    
+if first_num == last_num:
+    return first_num
+else:
+    return (first_num+last_num)/2
+    
 print(numbers)
